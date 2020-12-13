d62d2ce1c3a99ba4a51bba4a217381c5838c008a
akio0911 <akio0911@gmail.com>
2020-12-13 16:03:51 +0900

    feedback

--- kadai6/ViewController.swift
+++ kadai6/ViewController.swift
@@ -11,30 +11,45 @@ class ViewController: UIViewController {
     @IBOutlet private var randomLabel: UILabel!
     @IBOutlet private var slider: UISlider!
     
+    // 答えはLabelで持たず、プロパティとして持ちましょう。
+    // 例えば答えを漢字でラベルに表示する（例：二十四）という仕様変更があった場合、
+    // ラベルから答えを取り出すのが困難になってしまいます。
+    private var answer: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        random()
+        resetAnswer()
+        updateLabel()
     }
     
     @IBAction func judgeButton(_ sender: Any) {
-        let sliderValue = String(Int(slider.value))
-        guard randomLabel.text != sliderValue else {
-            judge(message: "あたり！あなたの値は\(sliderValue)")
-            random()
-            return
+        let message: String
+        
+        let sliderValue = Int(slider.value)
+        
+        if sliderValue == answer {
+            message = "あたり！あなたの値は\(sliderValue)"
+        } else {
+            message = "ハズレ！あなたの値は\(sliderValue)"
         }
-        judge(message: "ハズレ！あなたの値は\(sliderValue)")
-        random()
+        
+        presentJudgment(message: message)
+        
+        resetAnswer()
+        updateLabel()
     }
     
-    private func judge(message: String) {
+    private func presentJudgment(message: String) {
         let alert = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "再挑戦", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
     
-    private func random() {
-        randomLabel.text = String(arc4random_uniform(100) + 1)
+    private func updateLabel() {
+        randomLabel.text = String(answer)
+    }
+    
+    private func resetAnswer() {
+        answer = Int(arc4random_uniform(100) + 1)
     }
 }
-
